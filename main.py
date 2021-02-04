dfdac03abe5af950f41aafa649cec1a478ecf8a5
Vadim Vergasov <vadim.vergasov2003@gmail.com>
2021-02-04 22:07:24 +0300

    Rewrote bot to use undocumented API
    Thanks to @Neitex

--- main.py
+++ main.py
@@ -18,20 +18,35 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import datetime
 import json
 import os
-import re
 
 import requests
 import telebot
-from lxml import etree, html
 
 import config
 
-DATES = {
-    "01.09.20": "30.10.20",
-    "09.11.20": "27.12.20",
-    "11.01.21": "28.03.21",
-    "05.04.21": "31.05.21",
-}
+if not os.path.isfile("database.json"):
+    with open("database.json", "w") as f:
+        f.write("{}")
+with open("database.json", "r", encoding="utf-8") as f:
+    TOKENS = json.load(f)
+
+
+def get_token(username, password):
+    tries = 0
+    request = requests.post(
+        "https://schools.by/api/auth",
+        data={"username": username, "password": password},
+    )
+    while tries < 10 and request.status_code == 200:
+        request = requests.post(
+            "https://schools.by/api/auth",
+            data={"username": username, "password": password},
+        )
+        tries += 1
+    if request.status_code != 200:
+        return "Retry later"
+    token = request.json()["token"]
+    return token
 
 
 def write_to_log(message):
@@ -47,14 +62,6 @@ def check_date(date):
         datetime.datetime.strptime(date, "%d.%m.%y")
     except ValueError:
         return config.INCORRECT_FORMAT
-    valid = False
-    for s in DATES.keys():
-        start = datetime.datetime.strptime(s, "%d.%m.%y")
-        end = datetime.datetime.strptime(DATES[s], "%d.%m.%y")
-        if start <= datetime.datetime.strptime(date, "%d.%m.%y") <= end:
-            valid = True
-    if not valid:
-        return config.NOT_VALID
     if datetime.datetime.strptime(date, "%d.%m.%y").weekday() > 4:
         return config.NOT_VALID
     return "OK"
@@ -63,164 +70,177 @@ def check_date(date):
 def get_ht(date, message):
     string = ""
     year, month, day = (
-        int("20" + str(date.split(".")[2])),
-        int(date.split(".")[1]),
-        int(date.split(".")[0]),
+        "20" + str(date.split(".")[2]),
+        date.split(".")[1],
+        date.split(".")[0],
     )
-    day = datetime.datetime(year, month, day)
-    monday = str(day - datetime.timedelta(days=day.weekday())).split(" ")[0]
 
-    quarter = 0
-
-    if 8 < month < 11:
-        quarter = 40
-    elif 10 < month < 13:
-        quarter = 42
-    elif 0 < month < 4:
-        quarter = 43
-    elif 3 < month < 6:
-        quarter = 44
-
-    user_info = None
-    if not os.path.isfile("database.json"):
-        with open("database.json", "w") as f:
-            f.write("{}")
-    with open("database.json", "r", encoding="utf-8") as f:
-        user_info = json.load(f)
-    if not str(message.chat.id) in user_info.keys():
-        return config.NO_INFO
-
-    headers = config.HEADERS
-    pupil_id = None
-    if str(message.from_user.id) in user_info[str(message.chat.id)].keys():
-        pupil_id = str(user_info[str(message.chat.id)][str(message.from_user.id)]["id"])
-        headers["cookie"] = str(
-            user_info[str(message.chat.id)][str(message.from_user.id)]["cookie"]
-        )
+    token = None
+    if not str(message.from_user.id) in TOKENS.keys():
+        if not str(message.chat.id) in TOKENS.keys():
+            return config.NO_INFO
+        else:
+            token = TOKENS[str(message.chat.id)]
     else:
-        pupil_id = str(
-            user_info[str(message.chat.id)][
-                list(user_info[str(message.chat.id)].keys())[0]
-            ]["id"]
+        token = str(message.from_user.id)
+
+    headers = {"Authorization": "Token " + token + " "}
+
+    request = requests.get(
+        "https://schools.by/subdomain-api/user/current", headers=headers
+    )
+    tries = 0
+    while request.status_code != 200 and tries < 10:
+        request = requests.get(
+            "https://schools.by/subdomain-api/user/current", headers=headers
         )
-        headers["cookie"] = str(
-            user_info[str(message.chat.id)][
-                list(user_info[str(message.chat.id)].keys())[0]
-            ]["cookie"]
-        )
+        tries += 1
+    if request.status_code != 200:
+        return config.SOMETHING_WENT_WRONG
+    user_info = request.json()
+
+    pupil_id = user_info["id"]
 
     try:
         r = requests.get(
-            "https://gymn29.schools.by/pupil/"
-            + pupil_id
-            + "/dnevnik/quarter/"
-            + str(quarter)
-            + "/week/"
-            + str(monday),
+            "https://schools.by/subdomain-api/pupil/"
+            + str(pupil_id)
+            + "/daybook/day/"
+            + str(year + "-" + month + "-" + day),
             headers=headers,
         )
     except requests.exceptions.ConnectionError:
         try:
             r = requests.get(
-                "https://gymn29.schools.by/pupil/"
-                + pupil_id
-                + "/dnevnik/quarter/"
-                + str(quarter)
-                + "/week/"
-                + str(monday),
+                "https://schools.by/subdomain-api/pupil/"
+                + str(pupil_id)
+                + "/daybook/day/"
+                + str(year + "-" + month + "-" + day),
                 headers=headers,
             )
         except requests.exceptions.ConnectionError as error:
             write_to_log("RequestsError: {0}".format(error))
             return config.SOMETHING_WENT_WRONG
 
-    try:
-        tree = html.fromstring(r.text)
-    except etree.ParserError:
-        return config.NO_DATE
+    hometask = r.json()
 
-    date_str = "db_table_" + date
-
-    xpath = "//*[@id='" + date_str + "']/tbody/tr"
-
-    for row in tree.xpath(xpath):
-        if (
-            len(
-                [
-                    x
-                    for x in re.sub(
-                        "\n",
-                        "",
-                        row.xpath("td[1]/span")[0].text_content().strip().strip(),
-                    ).split(" ")
-                    if x != ""
-                ]
-            )
-            > 1
-        ):
-            string += "`" + (
-                " ".join(
-                    [
-                        x
-                        for x in re.sub(
-                            "\n",
-                            "",
-                            row.xpath("td[1]/span")[0].text_content().strip().strip(),
-                        ).split(" ")
-                        if x != ""
-                    ]
-                )
-                + ": "
-            )
-            try:
-                string += row.xpath("td[2]/div[2]/div")[0].text_content().strip()
-            except IndexError:
-                string += "Ничего"
-            try:
-                string += (
-                    "`\n[Файлы](https://gymn29.schools.by"
-                    + str(row.xpath("td[2]/div[1]/div[1]/a[1]/@href")[0])
-                    + ")\n"
-                )
-            except IndexError:
+    for row in hometask["lessons"].keys():
+        string += "`" + row + ". " + hometask["lessons"][row]["subject"] + ": "
+        if hometask["lessons"][row]["lesson_data"]["hometask"] == None:
+            string += "Ничего\n`"
+        else:
+            string += hometask["lessons"][row]["lesson_data"]["hometask"]["text"]
+            if (
+                len(hometask["lessons"][row]["lesson_data"]["hometask"]["attachments"])
+                != 0
+            ):
+                string += "`"
+                for attachment in hometask["lessons"][row]["lesson_data"]["hometask"][
+                    "attachments"
+                ]:
+                    string += "\n[Файл](" + str(attachment["file"]) + ")\n"
+            else:
                 string += "`\n"
-    return "" + string + "\n"
+    return string
 
 
 BOT = telebot.TeleBot(config.TG_TOKEN, parse_mode="MARKDOWN")
 
 
+def check(message):
+    """
+    Check if admin
+    """
+    id = BOT.get_me().id
+    if (
+        BOT.get_chat_member(message.chat.id, str(id)).can_delete_messages
+        or message.chat.type == "private"
+    ):
+        return True
+    BOT.reply_to(message, config.NO_ADMIN)
+    return False
+
+
+def check_for_creds(message):
+    if len(message.text.split(" ")) == 2:
+        try:
+            message.reply_to_message.chat.id
+        except AttributeError:
+            return False
+        return True
+    return False
+
+
 @BOT.message_handler(commands=["start", "help"])
 def info(message):
+    if not check(message):
+        return
     BOT.reply_to(message, config.ABOUT, disable_notification=True)
+
+
+@BOT.message_handler(func=check_for_creds)
+def getting_token(message):
+    token = get_token(*message.text.split(" "))
+    if token == "Retry later":
+        BOT.send_message(message.chat.id, config.RETRY_LATER)
+        return
+    TOKENS[str(message.from_user.id)] = token
+    tries = 0
+    headers = {"Authorization": "Token " + TOKENS[str(message.from_user.id)] + " "}
+    request = requests.get(
+        "https://schools.by/subdomain-api/user/current", headers=headers
+    )
+    while request.status_code != 200 and tries < 10:
+        request = requests.get(
+            "https://schools.by/subdomain-api/user/current", headers=headers
+        )
+        tries += 1
+    if request.status_code != 200:
+        return config.SOMETHING_WENT_WRONG
+    user_info = request.json()
+    BOT.send_message(
+        message.chat.id,
+        config.LOGGED_IN.format(
+            user_info["last_name"], user_info["first_name"], user_info["subdomain"]
+        ),
+    )
+    with open("database.json", "w") as fl:
+        json.dump(TOKENS, fl)
+    if message.chat.type != "private":
+        BOT.delete_message(
+            message.reply_to_message.chat.id,
+            message.reply_to_message.message_id,
+        )
+        BOT.delete_message(
+            message.chat.id,
+            message.message_id,
+        )
+
+
+@BOT.message_handler(commands=["login"])
+def login(message):
+    if not check(message):
+        return
+    BOT.reply_to(message, config.LOGIN_TEXT, disable_notification=True)
 
 
 @BOT.message_handler(commands=["set"])
 def add_member(message):
-    if len(message.text.split(" ")) < 3:
-        BOT.reply_to(message, config.INCORRECT_FORMAT, disable_notification=True)
+    if not check(message):
         return
-    current_chats = None
-    if not os.path.isfile("database.json"):
-        with open("database.json", "w") as f:
-            f.write("{}")
-    with open("database.json", "r", encoding="utf-8") as f:
-        current_chats = json.load(f)
-    current_chats[str(message.chat.id)] = {
-        message.from_user.id: {
-            "id": message.text.split(" ")[1],
-            "cookie": str(message.text.split(" ")[2:])
-            .replace(",", "")
-            .replace("'", "")[1:-1],
-        }
-    }
-    with open("database.json", "w") as f:
-        json.dump(current_chats, f)
+    if not str(message.from_user.id) in TOKENS.keys():
+        BOT.reply_to(message, config.NO_INFO)
+        return
+    TOKENS[str(message.chat.id)] = TOKENS[str(message.from_user.id)]
+    with open("database.json", "w") as fl:
+        json.dump(TOKENS, fl)
     BOT.reply_to(message, "Ok", disable_notification=True)
 
 
 @BOT.message_handler(commands=["hometask"])
 def send_hometask(message):
+    if not check(message):
+        return
     today = datetime.date.today()
     start_of_week = today - datetime.timedelta(days=today.weekday())  # Monday
     keyboard = telebot.types.InlineKeyboardMarkup()
